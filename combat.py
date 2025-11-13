5c714ecec6e7e181a3db0c5058618b1a26a09435
BrutalRemains <kristianscottroberts@gmail.com>
2025-11-13 10:33:54 -0500

    Updated  combat class logic

--- combat.py
+++ combat.py
@@ -2,10 +2,11 @@ import random
 
 class Commbat:
     def __init__(self, player, npc):
-        self.player = player
+        self.player = player # player and npc object
         self.npc = npc
-        self.player_meter = 0
+        self.player_meter = 0  # meter is an under the hood mechanic based on the speed of the weapon. 
         self.npc_meter = 0
+        self.meter_threshold = 10 # once this threshold is hit, player or npc will get an extra turn.
 
     def resolve_attack(self, attacker, defender):
         weapon = attacker.weapon
@@ -28,5 +29,20 @@ class Commbat:
         else:
             print(f"The attack misses!")
     
-    def simulate_duel(self, attack):
-        return False
+    def simulate_duel(self):
+        print("Now! Fight for the Great Smith!")
+
+        while self.player.is_alive() and self.npc.is_alive():
+            self.player_meter += self.player.weapon.speed
+            self.npc_meter += self.npc.weapon.speed
+
+            if self.player_meter >= self.meter_threshold:
+                self.resolve_attack(self.player, self.npc)
+                self.player_meter -= self.meter_threshold 
+
+            if self.npc.is_alive() and self.npc_meter >= self.meter_threshold:
+                self.resolve_attack(self.npc, self.player)
+                self.npc_meter -= self.meter_threshold
+            
+            winner = self.player if self.player.is_alive() else self.npc
+            return winner
